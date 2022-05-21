69bb83b4ed27dce753307c3d7cdc4cfd71c5f34e
Daniil_Shakhmovedev <Daniil_Shakhmovedev@epam.com>
2022-05-21 21:02:39 +0300

    COMPLETE FULL PART #2

--- src/main/java/com/example/labproject/ejb/SelectBean.java
+++ src/main/java/com/example/labproject/ejb/SelectBean.java
@@ -1,11 +1,76 @@
 package com.example.labproject.ejb;
 
+import com.example.labproject.models.Address;
+import com.example.labproject.storage.ClientStorage;
+
 import javax.ejb.Stateless;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Stateless
 public class SelectBean {
 
+    public void findAllClients(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("clientList", ClientStorage.CLIENT_LIST);
+        request.getRequestDispatcher("/view-list.jsp").forward(request, response);
+    }
 
+    public void findAllUseFilter(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            String city,
+            String street,
+            String num
+    ) throws ServletException, IOException {
+        if (city.length() > 101) {
+            request.setAttribute("errorField", "город фильтра");
+            request.setAttribute("errorReason", "полее превышает допустимую длину");
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+            return;
+        } else if (city.replaceAll("[a-zA-Z]", "").length() != city.length()) {
+            request.setAttribute("errorField", "город фильтра");
+            request.setAttribute("errorReason", "полее содержит латинский алфавит");
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+            return;
+        }
 
+        if (street.length() > 101) {
+            request.setAttribute("errorField", "улица фильтра");
+            request.setAttribute("errorReason", "полее превышает допустимую длину");
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+            return;
+        } else if (street.replaceAll("[a-zA-Z]", "").length() != street.length()) {
+            request.setAttribute("errorField", "улица фильтра");
+            request.setAttribute("errorReason", "полее содержит латинский алфавит");
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+            return;
+        }
 
+        try {
+            if (!num.trim().equals("")) Integer.parseInt(num);
+        } catch (ClassCastException e) {
+            request.setAttribute("errorField", "номер дома фильтра");
+            request.setAttribute("errorReason", "не число");
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+            return;
+        }
+
+        request.setAttribute("clientList",
+                ClientStorage.CLIENT_LIST.stream()
+                        .filter(Objects::nonNull)
+                        .filter(client ->
+                                client.getAddressList().contains(
+                                        client.getAddressList().stream().filter(
+                                                address -> (city.trim().equals("") || address.getCity().equals(city)) &&
+                                                        (street.trim().equals("") || address.getStreet().equals(street)) &&
+                                                        (num.trim().equals("") || address.getNum() == Integer.parseInt(num))
+                                        ).findFirst().orElse(new Address()))
+                        )
+                        .collect(Collectors.toList()));
+        request.getRequestDispatcher("/view-list.jsp").forward(request, response);
+    }
 }
--- src/main/java/com/example/labproject/servlets/ViewList.java
+++ src/main/java/com/example/labproject/servlets/ViewList.java
@@ -1,26 +1,28 @@
 package com.example.labproject.servlets;
 
+import com.example.labproject.ejb.SelectBean;
 import com.example.labproject.models.Address;
-import com.example.labproject.models.Client;
 import com.example.labproject.storage.ClientStorage;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @WebServlet(name = "ViewList", value = "/viewlist")
 public class ViewList extends HttpServlet {
 
+    @EJB
+    private SelectBean selectBean;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("clientList", ClientStorage.CLIENT_LIST);
-        request.getRequestDispatcher("/view-list.jsp").forward(request, response);
+        selectBean.findAllClients(request,response);
     }
 
     @Override
@@ -32,52 +34,6 @@ public class ViewList extends HttpServlet {
         String street = request.getParameter("streetFilter");
         String num = request.getParameter("numFilter");
 
-
-        if (city.length()>101) {
-            request.setAttribute("errorField", "город фильтра");
-            request.setAttribute("errorReason", "полее превышает допустимую длину");
-            request.getRequestDispatcher("/error.jsp").forward(request, response);
-            return;
-        } else if (city.replaceAll("[a-zA-Z]", "").length() != city.length()) {
-            request.setAttribute("errorField", "город фильтра");
-            request.setAttribute("errorReason", "полее содержит латинский алфавит");
-            request.getRequestDispatcher("/error.jsp").forward(request, response);
-            return;
-        }
-
-        if (street.length()>101) {
-            request.setAttribute("errorField", "улица фильтра");
-            request.setAttribute("errorReason", "полее превышает допустимую длину");
-            request.getRequestDispatcher("/error.jsp").forward(request, response);
-            return;
-        } else if (street.replaceAll("[a-zA-Z]", "").length() != street.length()) {
-            request.setAttribute("errorField", "улица фильтра");
-            request.setAttribute("errorReason", "полее содержит латинский алфавит");
-            request.getRequestDispatcher("/error.jsp").forward(request, response);
-            return;
-        }
-
-        try {
-            if(!num.trim().equals("")) Integer.parseInt(num);
-        } catch (ClassCastException e) {
-            request.setAttribute("errorField", "номер дома фильтра");
-            request.setAttribute("errorReason", "не число");
-            request.getRequestDispatcher("/error.jsp").forward(request, response);
-            return;
-        }
-
-              request.setAttribute("clientList",
-                ClientStorage.CLIENT_LIST.stream()
-                        .filter(Objects::nonNull)
-                        .filter(client ->
-                                client.getAddressList().contains(
-                                        client.getAddressList().stream().filter(
-                                                        address ->  (city.trim().equals("") || address.getCity().equals(city)) &&
-                                                                    (street.trim().equals("") || address.getStreet().equals(street)) &&
-                                                                    (num.trim().equals("") || address.getNum() == Integer.parseInt(num))
-                                                        ).findFirst().orElse(new Address()))
-                        )
-                        .collect(Collectors.toList()));
-        request.getRequestDispatcher("/view-list.jsp").forward(request, response);
+        selectBean.findAllUseFilter(request,response,city,street,num);
     }
 }
